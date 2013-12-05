fee912ca116cc902cfbde9d08396f5d38c09fdc6
Dave Airlie <airlied@redhat.com>
2013-12-05 05:39:11 +0000

    Mesa 10.0 release branch git snapshot

--- sanitize-tarball.sh
+++ sanitize-tarball.sh
@@ -20,14 +20,9 @@ pushd $dirname
 
 cat > src/gallium/auxiliary/vl/vl_mpeg12_decoder.c << EOF
 #include "vl_mpeg12_decoder.h"
-struct pipe_video_decoder *
-vl_create_mpeg12_decoder(struct pipe_context *context,
-                         enum pipe_video_profile profile,
-			 enum pipe_video_entrypoint entrypoint,
-			 enum pipe_video_chroma_format chroma_format,
-			 unsigned width, unsigned height,
-			 unsigned max_references,
-			 bool expect_chunked_decode)
+struct pipe_video_codec *
+vl_create_mpeg12_decoder(struct pipe_context *pipe,
+			const struct pipe_video_codec *templat)
 {
     return NULL;
 }
@@ -36,17 +31,14 @@ EOF
 cat > src/gallium/auxiliary/vl/vl_decoder.c << EOF
 #include "vl_decoder.h"
 bool vl_profile_supported(struct pipe_screen *screen,
-                          enum pipe_video_profile profile)
+                          enum pipe_video_profile profile,
+			 enum pipe_video_entrypoint entrypoint)
 {
     return false;
 }
-struct pipe_video_decoder *
+struct pipe_video_codec *
 vl_create_decoder(struct pipe_context *pipe,
-                  enum pipe_video_profile profile,
-                  enum pipe_video_entrypoint entrypoint,
-                  enum pipe_video_chroma_format chroma_format,
-                  unsigned width, unsigned height, unsigned max_references,
-                  bool expect_chunked_decode)
+		const struct pipe_video_codec *templat)
 {
     return NULL;
 }
